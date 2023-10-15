cc2a0a38556bd9881e28419cb006c5642a1763c1
Odiljon <bobosodikov0d@gmail.com>
2023-10-15 10:29:29 -0700

    project completed

---
+++ MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿namespace BillSplitter;
+
+public partial class MainPage : ContentPage
+{
+	private decimal bill;
+	private int tip;
+	private int noPeople = 1;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void txtBill_Completed(object sender, EventArgs e)
+    {
+		bill = decimal.Parse(txtBill.Text);
+		CalculateTotal();
+    }
+
+    private void CalculateTotal()
+    {
+        var totalTip = (bill * tip) / 100;
+        var tipByPerson = totalTip / noPeople;
+        
+        lbTipByPerson.Text = $"{tipByPerson:C}";
+
+        var subtotal = bill / noPeople;
+
+        lbSubtotal.Text = $"{subtotal:C}";
+
+        var totalByPerson = (bill + totalTip) / noPeople;
+        lbTotal.Text = $"{totalByPerson:C}";
+    }
+
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        tip = (int)sldTip.Value;
+        lblTip.Text = $"Tip: {tip}%";
+        CalculateTotal();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if (sender is Button)
+        {
+            var btn = (Button)sender;
+            var percentage = int.Parse(btn.Text.Replace("%", ""));
+            sldTip.Value = percentage;
+        }
+    }
+
+    private void btnMinus_Clicked(object sender, EventArgs e)
+    {
+        if(noPeople == 1){
+            return;
+        }
+
+        noPeople--;
+        lblNoPersons.Text = noPeople.ToString();
+        CalculateTotal();
+    }
+
+    private void btnPlus_Clicked(object sender, EventArgs e)
+    {
+        noPeople++;
+        lblNoPersons.Text = noPeople.ToString();
+        CalculateTotal();
+    }
+}
+
---
+++ MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BillSplitter;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+        {
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
